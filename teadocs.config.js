65d852dd36c99c10b2167e3ae82299163d1c92d4
Lisniuse <17560235@qq.com>
2018-06-22 14:40:32 +0800

    Add comment system.

--- teadocs.config.js
+++ teadocs.config.js
@@ -16,8 +16,69 @@ module.exports = {
         headHtml: `
         <meta name="description" content="这是NumPy官方的中文文档，NumPy是用Python进行科学计算的基础软件包。" />
         <meta name="keywords" content="numpy中文文档, numpy官方中文文档, NumPy中文文档, NumPy教程, NumPy文档, NumPy, Python, Python科学计算, 机器学习科学计算, 深度学习科学计算" />
+        <link rel="shortcut icon" href="/static/favicon.ico"/>
         `,
-        footHtml: "",
+        footHtml: `
+        <script>
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?a809b6f7e6517af8c15c6076273e80fe";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+        </script>
+        <script>
+        (function() {
+            var cy = window.document.createElement("div");
+            cy.id = "SOHUCS";
+            cy.style.maxWidth = "900px";
+            cy.style.padding = "20px";  
+            cy.style.boxSizing = "border-box";
+            cy.style.margin = "0px";
+            cy.style.backgroundColor = "#fff";
+            document.querySelector(".tea-container").appendChild(cy);
+        })()
+        </script>
+        <script type = "text/javascript" > (function() {
+            var appid = 'cytG5UKih';
+            var conf = 'prod_34fc698f6822858dad2f00c2bc25354e';
+            var width = window.innerWidth || document.documentElement.clientWidth;
+            if (width < 960) {
+                window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\\\/script>');
+            } else {
+                var loadJs = function(d, a) {
+                    var c = document.getElementsByTagName("head")[0] || document.head || document.documentElement;
+                    var b = document.createElement("script");
+                    b.setAttribute("type", "text/javascript");
+                    b.setAttribute("charset", "UTF-8");
+                    b.setAttribute("src", d);
+                    if (typeof a === "function") {
+                        if (window.attachEvent) {
+                            b.onreadystatechange = function() {
+                                var e = b.readyState;
+                                if (e === "loaded" || e === "complete") {
+                                    b.onreadystatechange = null;
+                                    a();
+                                }
+                            }
+                        } else {
+                            b.onload = a;
+                        }
+                    }
+                    c.appendChild(b);
+                };
+                loadJs("https://changyan.sohu.com/upload/changyan.js",
+                function() {
+                    window.changyan.api.config({
+                        appid: appid,
+                        conf: conf
+                    })
+                });
+            }
+        })(); 
+        </script>
+        `,
         isMinify: true, 
         rootPath: "/"
     },
